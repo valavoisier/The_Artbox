55f6df17620c095de08ff4ee6b847c74f45a43da
Valerie Lavoisier <val.lavoisier3@gmail.com>
2025-08-04 19:12:36 +0200

    recupération de chacune des oeuvres via requete

--- oeuvre.php
+++ oeuvre.php
@@ -3,22 +3,13 @@
     require 'bdd.php';
 
     $bdd = connexion();
-    $oeuvres = $bdd->query("SELECT * FROM oeuvres");
+    $requete = $bdd->prepare("SELECT * FROM oeuvres WHERE id = ?");
+    $requete->execute([$_GET['id']]);
+    $oeuvre = $requete->fetch();
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
         header('Location: index.php');
-    }
-
-    $oeuvre = null;
-
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
     }
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
