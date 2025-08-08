0f439a4985ac95ee5e591a87be364ec064fb6f6c
Valerie Lavoisier <val.lavoisier3@gmail.com>
2025-08-08 11:38:43 +0200

    securité xss

--- oeuvre.php
+++ oeuvre.php
@@ -20,13 +20,13 @@
 
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?= htmlspecialchars($oeuvre['image']) ?>" alt="<?= htmlspecialchars($oeuvre['titre']) ?>">
     </div>
     <div id="contenu-oeuvre">
-        <h1><?= $oeuvre['titre'] ?></h1>
-        <p class="description"><?= $oeuvre['artiste'] ?></p>
+        <h1><?= htmlspecialchars($oeuvre['titre']) ?></h1>
+        <p class="description"><?= htmlspecialchars($oeuvre['artiste']) ?></p>
         <p class="description-complete">
-             <?= $oeuvre['description'] ?>
+             <?= htmlspecialchars($oeuvre['description']) ?>
         </p>
     </div>
 </article>
